73a58df73866726c53650aa36f0c370aab59ed32
finmcf <92210179+finmcf@users.noreply.github.com>
2023-07-10 11:15:15 +0100

    updates

--- SendFriendRequestButton.tsx
+++ SendFriendRequestButton.tsx
@@ -6,6 +6,8 @@ import { deleteField } from "firebase/firestore";
 
 const SendFriendRequestButton = ({ loggedInUserId, profileUserId }) => {
   const [requestSent, setRequestSent] = useState(false);
+  const [isFriend, setIsFriend] = useState(false);
+  const [requestReceived, setRequestReceived] = useState(false);
 
   useEffect(() => {
     const fetchFriendRequestStatus = async () => {
@@ -18,6 +20,13 @@ const SendFriendRequestButton = ({ loggedInUserId, profileUserId }) => {
           userData.friendRequestsSent &&
             userData.friendRequestsSent.hasOwnProperty(profileUserId)
         );
+        setIsFriend(
+          userData.friends && userData.friends.hasOwnProperty(profileUserId)
+        );
+        setRequestReceived(
+          userData.friendRequestsReceived &&
+            userData.friendRequestsReceived.hasOwnProperty(profileUserId)
+        );
       }
     };
 
@@ -25,6 +34,10 @@ const SendFriendRequestButton = ({ loggedInUserId, profileUserId }) => {
   }, [loggedInUserId, profileUserId]);
 
   const handleSendRequest = async () => {
+    if (requestSent || isFriend || requestReceived) {
+      return;
+    }
+
     const timestamp = new Date().toISOString();
     try {
       // Add to loggedInUser's sent requests
@@ -61,32 +74,73 @@ const SendFriendRequestButton = ({ loggedInUserId, profileUserId }) => {
     }
   };
 
+  const handleRemoveFriend = async () => {
+    try {
+      // Remove from loggedInUser's friends
+      await updateDoc(doc(db, "Users", loggedInUserId), {
+        [`friends.${profileUserId}`]: deleteField(),
+      });
+
+      // Remove from profileUser's friends
+      await updateDoc(doc(db, "Users", profileUserId), {
+        [`friends.${loggedInUserId}`]: deleteField(),
+      });
+
+      setIsFriend(false);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const handlePress = () => {
+    if (isFriend) {
+      Alert.alert(
+        "Remove Friend",
+        "Are you sure you want to remove this friend?",
+        [
+          {
+            text: "Yes",
+            onPress: handleRemoveFriend,
+            style: "destructive",
+          },
+          {
+            text: "No",
+            style: "cancel",
+          },
+        ],
+        { cancelable: false }
+      );
+    } else if (requestSent) {
+      Alert.alert(
+        "Cancel Friend Request",
+        "Do you want to cancel the friend request?",
+        [
+          {
+            text: "Cancel",
+            onPress: handleCancelRequest,
+            style: "cancel",
+          },
+          {
+            text: "No",
+            style: "destructive",
+          },
+        ],
+        { cancelable: false }
+      );
+    } else {
+      handleSendRequest();
+    }
+  };
+
   return (
-    <TouchableOpacity
-      onPress={() => {
-        if (requestSent) {
-          Alert.alert(
-            "Cancel Friend Request",
-            "Do you want to cancel the friend request?",
-            [
-              {
-                text: "Cancel",
-                onPress: handleCancelRequest,
-                style: "cancel",
-              },
-              {
-                text: "No",
-                style: "destructive",
-              },
-            ],
-            { cancelable: false }
-          );
-        } else {
-          handleSendRequest();
-        }
-      }}
-    >
-      <Text>{requestSent ? "Request Sent" : "Send Friend Request"}</Text>
+    <TouchableOpacity onPress={handlePress}>
+      <Text>
+        {isFriend
+          ? "Remove Friend"
+          : requestSent
+          ? "Request Sent"
+          : "Send Friend Request"}
+      </Text>
     </TouchableOpacity>
   );
 };
